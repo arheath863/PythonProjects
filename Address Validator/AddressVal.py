836193e001ac284ee0109b906b371027a5422884
Autumn Heath <arheath863@gmail.com>
2025-05-04 19:53:22 -0400

    added user input

--- Address Validator/AddressVal.py
+++ Address Validator/AddressVal.py
@@ -1,18 +1,17 @@
 # This program determines if an email address is valid seeking the characters "@", and "."
-
+def main ():
+    #User input
+    address = input("Please enter emailo address to check validity")
+    print ("The address entered is:" + address)
+    
 def addressVal(address):
     dot = address.find(".")
     at = address.find("@")
-    if (dot == -1):
-        print("Error")
+    if (dot ==-1):
+        print("Invalid")
     elif (at == -1):
-        print ("Error")
+        print("invalid")
     else:
-        print ("Valid")
+        print("valid")
 
-    print("This program will determine if input os a valid email address")
-    while(True):
-        print("A valid email address needs an '@' and an '.'")
-        x = input("Input yout email address:")
-
-        addressVal(x)
+    main()
